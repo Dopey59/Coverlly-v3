a1f036feb0988280f619a9861348063dd64fa241
Dopey59 <epibot59@gmail.com>
2024-12-18 18:42:59 +0100

    [~] Sitemap updated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,2 +1,8 @@
-export const siteUrl = 'https://www.coverlly.fr';
-export const exclude = ['/admin', '/drafts'];  
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://www.coverlly.fr', // URL de votre site
+    generateRobotsTxt: true, // Générer un fichier robots.txt
+  };
+  
+  export default config;
+  
