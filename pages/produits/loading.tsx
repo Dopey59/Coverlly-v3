513279fafafd0940ae4669635eb997ac2128720b
Dopey59 <william.penet@epitech.eu>
2024-12-26 13:20:26 +0100

    [+] React dom updated + carousel blog added, maybe hydra error fixed - finger cross

--- pages/produits/loading.tsx
+++ pages/produits/loading.tsx
@@ -1,25 +1,15 @@
 import React from "react";
-import {Card, Skeleton } from "@nextui-org/react";
 
-export default function LoadSkeleton() {
+export default function Loading() {
 
   return (
-    <Card className="w-[200px] space-y-5 p-4" radius="lg">
-      <Skeleton className="rounded-lg">
-        <div className="h-24 rounded-lg bg-default-300" />
-      </Skeleton>
-      <div className="space-y-3">
-        <Skeleton className="w-3/5 rounded-lg">
-          <div className="h-3 w-3/5 rounded-lg bg-default-200" />
-        </Skeleton>
-        <Skeleton className="w-4/5 rounded-lg">
-          <div className="h-3 w-4/5 rounded-lg bg-default-200" />
-        </Skeleton>
-        <Skeleton className="w-2/5 rounded-lg">
-          <div className="h-3 w-2/5 rounded-lg bg-default-300" />
-        </Skeleton>
+    <div className="">
+      <div className="h-screen flex justify-center items-center">
+        <div className='flex flex-col gap-4'>
+          <h1 className='md:text-5xl text-3xl'>Chargement des produits</h1>
+        </div>
       </div>
-    </Card>
+    </div>
   );
 }
 
